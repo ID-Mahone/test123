eda5b4ab2d21b7e7f54a4de80922c37ab843f34b
kevinkrips <83237983+kevinkrips@users.noreply.github.com>
2022-06-24 23:03:51 +0200

    corrected websync script

--- websync.sh
+++ websync.sh
@@ -1,5 +1,5 @@
 rm -r /var/www/html/
 mkdir /var/www/html/
-sudo chown -r www-data /var/www/html
-cp -r /home/ubuntu/test123 /var/www/html/
+sudo chown -c www-data /var/www/html
+cp -r /home/ubuntu/git/test123 /var/www/html/
 systemctl restart apache2
